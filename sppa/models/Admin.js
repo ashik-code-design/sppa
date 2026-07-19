178903be292a487adb9f08d159efa9debd07e264
ashik-code-design <ashikmoha333@gmail.com>
2026-07-19 19:50:11 +0530

    Create Admin.js

---
+++ sppa/models/Admin.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+  admin_id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Admin", adminSchema);
