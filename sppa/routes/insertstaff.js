77e725b763e573bc9c92e3526ac8e7301662b24e
ashik-code-design <ashikmoha333@gmail.com>
2026-07-22 21:33:42 +0530

    Update insertstaff.js

--- sppa/routes/insertstaff.js
+++ sppa/routes/insertstaff.js
@@ -6,54 +6,50 @@ dotenv.config();
 
 const Staff = require("./models/Staff");
 
-mongoose.connect(process.env.MONGO_URI)
-.then(async () => {
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(async () => {
     console.log("MongoDB Connected");
 
     const ids = [
-        "12FCS01",
-        "12FCS02",
-        "16FCS01",
-        "17FCS01",
-        "21FCS01",
-        "23FCS01",
-        "23FCS02",
-        "24FCS01",
-        "24FCS02",
-        "24FCS03",
-        "25FCS01",
-        "10TCS03",
-        "26SCS01"
+      "12FCS01",
+      "12FCS02",
+      "16FCS01",
+      "17FCS01",
+      "21FCS01",
+      "23FCS01",
+      "23FCS02",
+      "24FCS01",
+      "24FCS02",
+      "24FCS03",
+      "25FCS01",
+      "10TCS03",
+      "26SCS01"
     ];
 
+    // Hash the default password once
     const hashedPassword = await bcrypt.hash("12345678", 10);
 
     for (const id of ids) {
+      const exist = await Staff.findOne({ staff_id: id });
 
-        const exist = await Staff.findOne({ staff_id: id });
+      if (!exist) {
+        await Staff.create({
+          staff_id: id,
+          password: hashedPassword,
+          password_changed: false
+        });
 
-        if (!exist) {
-
-            await Staff.create({
-                staff_id: id,
-                password: hash,
-                password_changed: false
-            });
-
-            console.log(id + " inserted");
-
-        } else {
-
-            console.log(id + " already exists");
-
-        }
+        console.log(`${id} inserted`);
+      } else {
+        console.log(`${id} already exists`);
+      }
     }
 
     console.log("Finished");
-    process.exit();
-
-})
-.catch(err => {
-    console.log(err);
-});
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
