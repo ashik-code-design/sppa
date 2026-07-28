99094f65902e2e866264ee91b28e810d242d434c
ashik-code-design <ashikmoha333@gmail.com>
2026-07-28 15:41:25 +0530

    Update Admin.js

--- sppa/models/Admin.js
+++ sppa/models/Admin.js
@@ -4,11 +4,18 @@ const adminSchema = new mongoose.Schema({
   admin_id: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    uppercase: true,
+    trim: true
   },
   password: {
     type: String,
     required: true
+  },
+  role: {
+    type: String,
+    enum: ["admin", "staff"],
+    default: "staff"
   }
 });
 
