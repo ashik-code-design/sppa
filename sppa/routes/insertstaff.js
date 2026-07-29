6c48f6616ab9a140d021b38970ee1065c58ffff6
ashik-code-design <ashikmoha333@gmail.com>
2026-07-29 19:04:02 +0530

    Update insertstaff.js

--- sppa/routes/insertstaff.js
+++ sppa/routes/insertstaff.js
@@ -11,38 +11,80 @@ mongoose
   .then(async () => {
     console.log("MongoDB Connected");
 
-    const ids = [
-      "12FCS01",
-      "12FCS02",
-      "16FCS01",
-      "17FCS01",
-      "21FCS01",
-      "23FCS01",
-      "23FCS02",
-      "24FCS01",
-      "24FCS02",
-      "24FCS03",
-      "25FCS01",
-      "10TCS03",
-      "26SCS01"
+    const staffs = [
+      {
+        staff_id: "12FCS01",
+        staff_name: "Dr. S. BRITTO RAMESH KUMAR",
+      },
+      {
+        staff_id: "12FCS02",
+        staff_name: "Dr. A. ALOYSIUS",
+      },
+      {
+        staff_id: "16FCS01",
+        staff_name: "Dr. V. JUDE NIRMAL",
+      },
+      {
+        staff_id: "17FCS01",
+        staff_name: "Rev. Dr. S. ARUL OLI SJ",
+      },
+      {
+        staff_id: "21FCS01",
+        staff_name: "Rev. Dr. S. SANTIAGO SJ",
+      },
+      {
+        staff_id: "23FCS01",
+        staff_name: "Dr. A. VIMAL JERALD",
+      },
+      {
+        staff_id: "23FCS02",
+        staff_name: "Dr. GEORGE GABRIEL RICHARD ROY",
+      },
+      {
+        staff_id: "24FCS01",
+        staff_name: "Dr. A. ANGELPREETHI",
+      },
+      {
+        staff_id: "24FCS02",
+        staff_name: "Dr. G. AROCKIA SAHAYA SHEELA",
+      },
+      {
+        staff_id: "24FCS03",
+        staff_name: "Dr. P. JOSEPH CHARLES",
+      },
+      {
+        staff_id: "25FCS01",
+        staff_name: "Dr. A. JENIFER JOTHI MARY",
+      },
+      {
+        staff_id: "10TCS03",
+        staff_name: "Dr. K. MAHESWARAN",
+      },
+      {
+        staff_id: "26SCS01",
+        staff_name: "Mr. A. Shreenath",
+      },
     ];
 
-    // Hash the default password once
+    // Default password
     const hashedPassword = await bcrypt.hash("12345678", 10);
 
-    for (const id of ids) {
-      const exist = await Staff.findOne({ staff_id: id });
+    for (const staff of staffs) {
+      const exist = await Staff.findOne({
+        staff_id: staff.staff_id,
+      });
 
       if (!exist) {
         await Staff.create({
-          staff_id: id,
+          staff_id: staff.staff_id,
+          staff_name: staff.staff_name,
           password: hashedPassword,
-          password_changed: false
+          password_changed: false,
         });
 
-        console.log(`${id} inserted`);
+        console.log(`${staff.staff_id} inserted`);
       } else {
-        console.log(`${id} already exists`);
+        console.log(`${staff.staff_id} already exists`);
       }
     }
 
