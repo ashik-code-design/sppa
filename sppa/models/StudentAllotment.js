c463166a24aa57e03bfb75831801a203ff0a0a1a
ashik-code-design <ashikmoha333@gmail.com>
2026-07-29 19:39:04 +0530

    Update StudentAllotment.js

--- sppa/models/StudentAllotment.js
+++ sppa/models/StudentAllotment.js
@@ -1,47 +1,43 @@
 const mongoose = require("mongoose");
 
-const studentAllotmentSchema = new mongoose.Schema({
-
+const studentAllotmentSchema = new mongoose.Schema(
+  {
     department: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     section: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     subject: {
-        type: String,
-        required: true
-    },
-
-    staff_id: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     staff_name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     roll_from: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     roll_to: {
-        type: String,
-        required: true
-    }
-
-}, {
-    timestamps: true
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model(
-    "StudentAllotment",
-    studentAllotmentSchema
+  "StudentAllotment",
+  studentAllotmentSchema
 );
